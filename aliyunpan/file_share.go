787785f5790a2c15db6e518d31b4997741a45bab
xiaoyaofenfen <1254525673@qq.com>
2022-08-10 10:30:25 +0800

    support next page for share list

--- aliyunpan/file_share.go
+++ aliyunpan/file_share.go
@@ -7,57 +7,64 @@ import (
 	"github.com/tickstep/aliyunpan-api/aliyunpan/apiutil"
 	"github.com/tickstep/library-go/logger"
 	"strings"
+	"time"
 )
 
-type(
+type (
 	ShareEntity struct {
-		Creator       string    `json:"creator"`
-		DriveId       string    `json:"drive_id"`
-		ShareId       string    `json:"share_id"`
-		ShareName     string    `json:"share_name"`
+		Creator   string `json:"creator"`
+		DriveId   string `json:"drive_id"`
+		ShareId   string `json:"share_id"`
+		ShareName string `json:"share_name"`
 		// SharePwd 密码，为空代表没有密码
-		SharePwd      string    `json:"share_pwd"`
-		ShareUrl      string    `json:"share_url"`
-		FileIdList    []string  `json:"file_id_list"`
-		SaveCount     int       `json:"save_count"`
+		SharePwd   string   `json:"share_pwd"`
+		ShareUrl   string   `json:"share_url"`
+		FileIdList []string `json:"file_id_list"`
+		SaveCount  int      `json:"save_count"`
 		// Expiration 过期时间，为空代表永不过期
-		Expiration    string `json:"expiration"`
-		UpdatedAt     string `json:"updated_at"`
-		CreatedAt     string `json:"created_at"`
-		Status        string    `json:"status"`
-		FirstFile     *FileEntity `json:"first_file"`
+		Expiration string      `json:"expiration"`
+		UpdatedAt  string      `json:"updated_at"`
+		CreatedAt  string      `json:"created_at"`
+		Status     string      `json:"status"`
+		FirstFile  *FileEntity `json:"first_file"`
 	}
 
 	shareEntityResult struct {
-		CreatedAt     string `json:"created_at"`
-		Creator       string    `json:"creator"`
-		Description   string    `json:"description"`
+		CreatedAt   string `json:"created_at"`
+		Creator     string `json:"creator"`
+		Description string `json:"description"`
 		// 下载次数
-		DownloadCount int       `json:"download_count"`
-		DriveId       string    `json:"drive_id"`
-		Expiration    string `json:"expiration"`
-		Expired       bool      `json:"expired"`
-		FileId        string    `json:"file_id"`
-		FileIdList    []string  `json:"file_id_list"`
+		DownloadCount int      `json:"download_count"`
+		DriveId       string   `json:"drive_id"`
+		Expiration    string   `json:"expiration"`
+		Expired       bool     `json:"expired"`
+		FileId        string   `json:"file_id"`
+		FileIdList    []string `json:"file_id_list"`
 		// 浏览次数
-		PreviewCount  int       `json:"preview_count"`
+		PreviewCount int `json:"preview_count"`
 		// 转存次数
-		SaveCount     int       `json:"save_count"`
-		ShareId       string    `json:"share_id"`
-		ShareMsg      string    `json:"share_msg"`
-		ShareName     string    `json:"share_name"`
-		SharePolicy   string    `json:"share_policy"`
-		SharePwd      string    `json:"share_pwd"`
-		ShareUrl      string    `json:"share_url"`
-		Status        string    `json:"status"`
-		UpdatedAt     string `json:"updated_at"`
+		SaveCount   int    `json:"save_count"`
+		ShareId     string `json:"share_id"`
+		ShareMsg    string `json:"share_msg"`
+		ShareName   string `json:"share_name"`
+		SharePolicy string `json:"share_policy"`
+		SharePwd    string `json:"share_pwd"`
+		ShareUrl    string `json:"share_url"`
+		Status      string `json:"status"`
+		UpdatedAt   string `json:"updated_at"`
 
-		FirstFile     *fileEntityResult `json:"first_file"`
+		FirstFile *fileEntityResult `json:"first_file"`
 	}
 
-	shareListResult struct {
-		Items []*shareEntityResult `json:"items"`
-		NextMarker string `json:"next_marker"`
+	ShareListParam struct {
+		Creator string `json:"creator"`
+		Limit   int64  `json:"limit"`
+		Marker  string `json:"marker"`
+	}
+
+	ShareListResult struct {
+		Items      []*shareEntityResult `json:"items"`
+		NextMarker string               `json:"next_marker"`
 	}
 
 	ShareCancelResult struct {
@@ -69,9 +76,9 @@ type(
 
 	// 创建分享
 	ShareCreateParam struct {
-		DriveId    string   `json:"drive_id"`
+		DriveId string `json:"drive_id"`
 		// 分享密码，4个字符，为空代码公开分享
-		SharePwd   string   `json:"share_pwd"`
+		SharePwd string `json:"share_pwd"`
 		// 过期时间，为空代表永不过期。时间格式必须是这种：2021-07-23 09:22:19
 		Expiration string   `json:"expiration"`
 		FileIdList []string `json:"file_id_list"`
@@ -83,31 +90,46 @@ func createShareEntity(item *shareEntityResult) *ShareEntity {
 		return nil
 	}
 	return &ShareEntity{
-		Creator: item.Creator,
-		DriveId: item.DriveId,
-		ShareId: item.ShareId,
-		ShareName: item.ShareName,
-		SharePwd: item.SharePwd,
-		ShareUrl: item.ShareUrl,
+		Creator:    item.Creator,
+		DriveId:    item.DriveId,
+		ShareId:    item.ShareId,
+		ShareName:  item.ShareName,
+		SharePwd:   item.SharePwd,
+		ShareUrl:   item.ShareUrl,
 		FileIdList: item.FileIdList,
-		SaveCount: item.SaveCount,
-		Status: item.Status,
+		SaveCount:  item.SaveCount,
+		Status:     item.Status,
 		Expiration: apiutil.UtcTime2LocalFormat(item.Expiration),
-		UpdatedAt: apiutil.UtcTime2LocalFormat(item.UpdatedAt),
-		CreatedAt: apiutil.UtcTime2LocalFormat(item.CreatedAt),
-		FirstFile: createFileEntity(item.FirstFile),
+		UpdatedAt:  apiutil.UtcTime2LocalFormat(item.UpdatedAt),
+		CreatedAt:  apiutil.UtcTime2LocalFormat(item.CreatedAt),
+		FirstFile:  createFileEntity(item.FirstFile),
 	}
 }
 
-// ShareList 获取分享链接列表
+// ShareLinkList 获取所有分享链接列表
 func (p *PanClient) ShareLinkList(userId string) ([]*ShareEntity, *apierror.ApiError) {
 	resultList := []*ShareEntity{}
-	if r,e := p.getShareLinkListReq(userId); e == nil {
-		for _,item := range r.Items {
-			resultList = append(resultList, createShareEntity(item))
+	param := ShareListParam{
+		Creator: userId,
+		Limit:   100,
+		Marker:  "",
+	}
+	for {
+		if r, e := p.GetShareLinkListReq(param); e == nil {
+			for _, item := range r.Items {
+				resultList = append(resultList, createShareEntity(item))
+			}
+
+			// next page?
+			if r.NextMarker != "" {
+				param.Marker = r.NextMarker
+				time.Sleep(500 * time.Millisecond)
+			} else {
+				break
+			}
+		} else {
+			return nil, e
 		}
-	} else {
-		return nil, e
 	}
 	return resultList, nil
 }
@@ -121,15 +143,15 @@ func (p *PanClient) ShareLinkCancel(shareIdList []string) ([]*ShareCancelResult,
 
 	// param
 	pr := BatchRequestList{}
-	for _,shareId := range shareIdList {
+	for _, shareId := range shareIdList {
 		pr = append(pr, &BatchRequest{
-			Id:      shareId,
-			Method:  "POST",
-			Url:     "/share_link/cancel",
+			Id:     shareId,
+			Method: "POST",
+			Url:    "/share_link/cancel",
 			Headers: map[string]string{
 				"Content-Type": "application/json",
 			},
-			Body:    map[string]interface{}{
+			Body: map[string]interface{}{
 				"share_id": shareId,
 			},
 		})
@@ -141,7 +163,7 @@ func (p *PanClient) ShareLinkCancel(shareIdList []string) ([]*ShareCancelResult,
 	}
 
 	// request
-	result,err := p.BatchTask(fullUrl.String(), &batchParam)
+	result, err := p.BatchTask(fullUrl.String(), &batchParam)
 	if err != nil {
 		logger.Verboseln("share cancel error ", err)
 		return nil, apierror.NewFailedApiError(err.Error())
@@ -149,9 +171,9 @@ func (p *PanClient) ShareLinkCancel(shareIdList []string) ([]*ShareCancelResult,
 
 	// parse result
 	r := []*ShareCancelResult{}
-	for _,item := range result.Responses{
+	for _, item := range result.Responses {
 		r = append(r, &ShareCancelResult{
-			Id: item.Id,
+			Id:      item.Id,
 			Success: item.Status == 204,
 		})
 	}
@@ -161,7 +183,7 @@ func (p *PanClient) ShareLinkCancel(shareIdList []string) ([]*ShareCancelResult,
 // ShareLinkCreate 创建分享
 func (p *PanClient) ShareLinkCreate(param ShareCreateParam) (*ShareEntity, *apierror.ApiError) {
 	// header
-	header := map[string]string {
+	header := map[string]string{
 		"authorization": p.webToken.GetAuthorizationStr(),
 	}
 
@@ -205,27 +227,36 @@ func (p *PanClient) ShareLinkCreate(param ShareCreateParam) (*ShareEntity, *apie
 	return createShareEntity(r), nil
 }
 
-func (p *PanClient) getShareLinkListReq(userId string) (*shareListResult, *apierror.ApiError) {
+func (p *PanClient) GetShareLinkListReq(param ShareListParam) (*ShareListResult, *apierror.ApiError) {
 	// header
-	header := map[string]string {
+	header := map[string]string{
 		"authorization": p.webToken.GetAuthorizationStr(),
 	}
 
 	// url
 	fullUrl := &strings.Builder{}
-	fmt.Fprintf(fullUrl, "%s/adrive/v2/share_link/list", API_URL)
+	fmt.Fprintf(fullUrl, "%s/adrive/v3/share_link/list", API_URL)
 	logger.Verboseln("do request url: " + fullUrl.String())
 
+	if param.Limit <= 0 {
+		param.Limit = 100
+	}
 	// data
-	postData := map[string]interface{} {
-		"creator": userId,
+	postData := map[string]interface{}{
+		"category":         "file,album",
+		"creator":          param.Creator,
 		"include_canceled": false,
-		"order_by": "created_at",
-		"order_direction": "DESC",
+		"order_by":         "created_at",
+		"order_direction":  "DESC",
+		"limit":            param.Limit,
+	}
+	if param.Marker != "" {
+		postData["marker"] = param.Marker
 	}
 
 	// request
 	body, err := client.Fetch("POST", fullUrl.String(), postData, apiutil.AddCommonHeader(header))
+	logger.Verboseln(string(body))
 	if err != nil {
 		logger.Verboseln("get share list error ", err)
 		return nil, apierror.NewFailedApiError(err.Error())
@@ -237,7 +268,7 @@ func (p *PanClient) getShareLinkListReq(userId string) (*shareListResult, *apier
 	}
 
 	// parse result
-	r := &shareListResult{}
+	r := &ShareListResult{}
 	if err2 := json.Unmarshal(body, r); err2 != nil {
 		logger.Verboseln("parse share list result json error ", err2)
 		return nil, apierror.NewFailedApiError(err2.Error())
