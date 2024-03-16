ebf44b40ab1ed40aadf726d99f189661940fa832
tickstep <tickstep@outlook.com>
2024-03-16 14:18:13 +0800

    add file delete completely api

--- aliyunpan_open/file_delete.go
+++ aliyunpan_open/file_delete.go
@@ -29,3 +29,27 @@ RetryBegin:
 		}
 	}
 }
+
+// FileDeleteCompletely 彻底删除文件，不经回收站直接永久删除文件
+func (p *OpenPanClient) FileDeleteCompletely(param *aliyunpan.FileBatchActionParam) (*aliyunpan.FileBatchActionResult, *apierror.ApiError) {
+	retryTime := 0
+
+RetryBegin:
+	opParam := &openapi.FileIdentityPair{
+		DriveId: param.DriveId,
+		FileId:  param.FileId,
+	}
+	if result, err := p.apiClient.FileDelete(opParam); err == nil {
+		return &aliyunpan.FileBatchActionResult{
+			FileId:  result.FileId,
+			Success: true,
+		}, nil
+	} else {
+		// handle common error
+		if apiErrorHandleResp := p.HandleAliApiError(err, &retryTime); apiErrorHandleResp.NeedRetry {
+			goto RetryBegin
+		} else {
+			return nil, apiErrorHandleResp.ApiErr
+		}
+	}
+}
